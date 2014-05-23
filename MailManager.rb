e0dadbeca9cafbb71a34b8f7cfc19a8be5f0e176
Dan Walker <dan.walker@gmail.com>
2014-05-22 22:47:31 -0700

    Another attempt at URLs that work on mac and iOS

--- MailManager.rb
+++ MailManager.rb
@@ -169,8 +169,9 @@ def send_mail_drop_messages (imap_authenticated_connection, smtp_config, mailbox
     else
       subject = "#{email_prefix} #{subject}"
     end
-    plain_text = envelope.message_id.sub(/<(.*)\>/i, "message://%3C\\1%3E\r\n\r\n")
-    html_text = envelope.message_id.sub(/<(.*)\>/i, "message:&lt;\\1&gt; <message://%3C\\1%3E>\r\n<br>\r\n<br>\r\n")
+    plain_text = envelope.message_id.sub(/<(.*)\>/i, "message:<\\1>\r\n\r\n")
+    html_text = envelope.message_id.sub(/<(.*)\>/i, "message:&lt;\\1&gt;\r\n<br>\r\n<br>\r\n")
+
     if boundary.nil?
       if content_transfer_encoding.include?("quoted-printable")
         plain_text = plain_text.gsub(/=/, "=3d").length > 74 ? plain_text.gsub(/=/, "=3d").insert(73, "=\r\n") : plain_text.gsub(/=/, "=3d")
